082e57bd49ea7dd3b28986bfeabcfb6d6f060426
bugvish <vish@buglabs.net>
2014-05-01 17:18:09 -0400

    Update renesas.js

--- renesas.js
+++ renesas.js
@@ -1,6 +1,4 @@
 setTimeout(function(){
-	SWARM.leave(["69df1aea11433b3f85d2ca6e9c3575a9c86f8182"]);
-	SWARM.join(["69df1aea11433b3f85d2ca6e9c3575a9c86f8182","5dbaf819af6eeec879a1a1d6c388664be4595bb3"]);
 	$('button#emailsubmit').click(function(e){
 		var emailaddy = $("#emailfield").val();
 	    	console.log(emailaddy);
