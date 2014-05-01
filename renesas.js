536bcd2cd9f9091e0594a3008ccf521af6ae0932
bugvish <vish@buglabs.net>
2014-04-30 21:07:59 -0400

    Update renesas.js

--- renesas.js
+++ renesas.js
@@ -1,4 +1,5 @@
-SWARM.join(["5dbaf819af6eeec879a1a1d6c388664be4595bb3"]);
+setTimeout(function(){SWARM.join(["5dbaf819af6eeec879a1a1d6c388664be4595bb3"]);},5000);
+
 
 function toggleLEDS() {
 	var toswarms = [{swarm: "5dbaf819af6eeec879a1a1d6c388664be4595bb3",resource: "714e1063eaf0f7980238040e777fbe543bc73fdc"}];	
