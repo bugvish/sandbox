8fa474f37e91b903e2f1a21dc420267a4f0b056f
bugvish <vish@buglabs.net>
2014-04-30 21:42:14 -0400

    Update renesas.js

--- renesas.js
+++ renesas.js
@@ -3,14 +3,14 @@ setTimeout(function(){
 	SWARM.join(["69df1aea11433b3f85d2ca6e9c3575a9c86f8182","5dbaf819af6eeec879a1a1d6c388664be4595bb3"]);},5000);
 
 
-function toggleLEDS() {
+window.toggleLEDS = function(){
 	var toswarms = [{swarm: "5dbaf819af6eeec879a1a1d6c388664be4595bb3",resource: "714e1063eaf0f7980238040e777fbe543bc73fdc"}];	
 	var message = {name: 'LED', feed:{}}; 
 	message.feed[10]=(true);
 	SWARM.send(message, toswarms);
 }
 
-function toggleBuzzer() {
+window.toggleBuzzer = function() {
 	 SWARM.send({
             name: 'Beep',
             feed: {
