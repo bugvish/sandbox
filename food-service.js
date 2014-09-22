94ceea47c145dd41b8fda60e2cb9a3bd34924993
bugvish <vish@buglabs.net>
2014-09-22 18:34:46 -0400

    Update food-service.js

--- food-service.js
+++ food-service.js
@@ -125,7 +125,7 @@ function changeStore() {
 function changeMachine(machine) {
 	freeboard.showLoadingIndicator(true);
 	setTimeout(function(){	
-		freeboard.setDatasourceSettings("Machine_1", {"datafile":"http://bugops.com/vzwdemos/"+machine+".json"});
+		freeboard.setDatasourceSettings("Machine_1", {"datafile":"https://raw.githubusercontent.com/bugvish/sandbox/master/"+machine+".json"});
 		freeboard.showLoadingIndicator(false);
 	},1200);
 }
