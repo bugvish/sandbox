f2546857c447a091395bc64ec22b4fe8ed8c83df
bugvish <vish@buglabs.net>
2014-09-26 14:33:12 -0400

    Update cokevendingdemo.js

--- cokevendingdemo.js
+++ cokevendingdemo.js
@@ -2,12 +2,12 @@ $.support.cors = true;
 
 setTimeout(function(){	
 	$('button#vendingButton').click(function(e){
-		dweetio.dweet_for('coke-vendingdemo-waltham', {"credit":200}, function(err, dweet){});
 		freeboard.showLoadingIndicator(true);
 		setTimeout(function(){	
 			freeboard.showLoadingIndicator(false);
-			freeboard.showDialog($("<div align='center'>Vending Machine Credit Sent!  Please make a selection.</div>"),"Success!","OK",null,function(){}); 
-		},7000);
+			datasources["VendingMachine"]["count"]++;
+			//INCREMENT 
+		},5000);
 	});	
 		
 },3000);
