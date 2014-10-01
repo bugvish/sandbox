43ab37ac0c694fc46d98dbf3f375c3dc4515ef3b
bugvish <vish@buglabs.net>
2014-10-01 16:03:27 -0400

    Update verizonDatasource.js

--- verizonDatasource.js
+++ verizonDatasource.js
@@ -189,5 +189,5 @@
 
 		// Here we call createRefreshTimer with our current settings, to kick things off, initially. Notice how we make use of one of the user defined settings that we setup earlier.
 		logIn(currentSettings.user_name,currentSettings.password);
-		updateRefresh(currentSettings.refresh * 1000);
+		updateRefresh(currentSettings.refresh_time * 1000);
 	}
