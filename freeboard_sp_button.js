7f17e34fe9360ad336ab77dac6fdc45c25fbaf1d
Vishal Kumar <vish@buglabs.net>
2018-06-18 07:25:51 -0700

    Add files via upload

---
+++ freeboard_sp_button.js
@@ -0,0 +1,3 @@
+setTimeout(function(){ 
+$('#add-pane').after('<li id="add-pane"><span class="glyphicon glyphicon-plus icon-red"></span><label onclick="window.open(\'http://localhost/~PeterSemmelhack/SP/sp_gallery.html\',\'mywindow\');">Add Signalpattern widget</label></li>');
+},2000);
